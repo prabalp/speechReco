43fef964db434b1a6b745e9a59c7f6dee50eb28f
prabalp <prabalpanda2008@gmail.com>
2021-10-15 17:11:31 +0530

    initial_working_model

--- main.py
+++ main.py
@@ -1,9 +1,29 @@
 import speech_recognition as sr
+import webbrowser
 
 r = sr.Recognizer()
 
-with sr.Microphone() as source:
-    print('Say something')
-    audio = r.listen(source)
-    voice = r.recognize_google(audio)
-    print(voice)
+
+def audio_data():
+    with sr.Microphone() as source:
+        audio = r.listen(source)
+        voice = ""
+        try:
+            voice = r.recognize_google(audio)
+        except sr.UnknownValueError:
+            print("Try Again")
+        except sr.RequestError:
+            print("Sorry, my service is down")
+        return voice
+
+
+def respond(voice):
+    if 'I am fine' in voice:
+        print('Good to hear that')
+    # if 'search' in voice:
+    #     search = audio_data('What do you want to search')
+
+
+print('Hello how are you?')
+voice = audio_data()
+respond(voice)
