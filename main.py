284a3203823ac5092db2b228b3aac49ddb2bc05c
prabalp <prabalpanda2008@gmail.com>
2021-10-16 22:46:43 +0530

    playsound

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
 import speech_recognition as sr
 import webbrowser
 import time
+import playsound
+import os
+import random
+from gtts import gTTS
+from time import ctime
 
 r = sr.Recognizer()
 
@@ -8,37 +13,47 @@ r = sr.Recognizer()
 def audio_data(ask=False):
     with sr.Microphone() as source:
         if ask:
-            print(ask)
+            a_speak(ask)
         audio = r.listen(source)
         voice = ""
         try:
             voice = r.recognize_google(audio)
         except sr.UnknownValueError:
-            print("Try Again")
+            a_speak("Try Again")
         except sr.RequestError:
-            print("Sorry, my service is down")
+            a_speak("Sorry, my service is down")
         return voice
+
+
+def a_speak(a_string):
+    tts = gTTS(text=a_string, lang="en")
+    r = random.randint(1, 10000000)
+    audio_file = 'audio-'+str(r)+'.mp3'
+    tts.save(audio_file)
+    playsound.playsound(audio_file)
+    print(a_string)
+    os.remove(audio_file)
 
 
 def respond(voice):
     if 'I am fine' in voice:
-        print('Good to hear that')
+        a_speak('Good to hear that')
     if 'search' in voice:
         search = audio_data('What do you want to search')
         url = "https:://google.com/search?q=" + search
         webbrowser.get().open(url)
-        print('Here is what I found for ' + search)
+        a_speak('Here is what I found for ' + search)
     if 'find location' in voice:
         location = audio_data('What is the location')
         url = "https:://google.nl/maps/place/" + location + '/&amp;'
         webbrowser.get().open(url)
-        print('Here is the location of ' + location)
+        a_speak('Here is the location of ' + location)
     if 'exit' in voice:
         exit()
 
 
 time.sleep(1)
-print('Hello how are you?')
+a_speak('Hello how are you?')
 while 1:
     voice = audio_data()
     respond(voice)
